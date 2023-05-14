923c4a214201137ae8dda19ef84b8aba247225e5
new name <shcherbakov.illya@list.ru>
2023-05-15 00:51:28 +0300

    avaNew

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ $('a[href^="#"').on('click', function() {
     });
     return false;
 });
+
+let photo = document.querySelector('.card-photo-img');
+let modal = document.querySelector('.modal__photo');
+let close = document.querySelector('.close');
+
+photo.addEventListener('click',openModal);
+
+function openModal(){
+        modal.classList.toggle('hide');
+        document.body.style.overflow='hidden';
+        
+}
+
+close.addEventListener('click',closeModal);
+
+
+function closeModal(){
+        modal.classList.toggle('hide');
+        document.body.style.overflow='';
+}
