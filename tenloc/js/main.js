4e76cf719600dd301accba908b67037222b8a188
new name <shcherbakov.illya@list.ru>
2023-09-15 00:16:37 +0300

    testovoe

--- tenloc/js/main.js
+++ tenloc/js/main.js
@@ -13,7 +13,32 @@ $('.blog__slider').slick({
     slidesToScroll: 1,
     centerMode: false,
     dots:false,
-    arrows: true
+    arrows: true,
+    responsive: [
+        {
+          breakpoint: 1139,
+          settings: {
+            infinite: false,
+            slidesToShow: 3
+          }
+        },
+        {
+            breakpoint: 721,
+            settings: {
+              infinite: true,
+              centerMode: true,
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 531,
+            settings: {
+              infinite: true,
+              centerMode: false,
+              slidesToShow: 1
+            }
+          }
+    ]
     
 });
 let panelBtn = document.querySelector('.header__list-help');
@@ -22,3 +47,17 @@ let panel = document.querySelector('.header__window');
 panelBtn.addEventListener('click', function(){
     panel.classList.toggle('visible');
 })
+
+const burgerBtn = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger__menu');
+const burgerClose = document.querySelector('.burger__menu-close');
+
+burgerBtn.addEventListener('click', openMenu);
+burgerClose.addEventListener('click', closeMenu);
+
+function openMenu(){
+    burgerMenu.classList.add('visible');
+}
+function closeMenu(){
+    burgerMenu.classList.remove('visible');
+}
