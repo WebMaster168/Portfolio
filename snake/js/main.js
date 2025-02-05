32b3360733c24503b8af8a53cceb4d83476bec40
new name <shcherbakov.illya@list.ru>
2025-02-05 23:26:12 +0400

    add movement

--- snake/js/main.js
+++ snake/js/main.js
@@ -14,11 +14,14 @@ let cycleGame = false;
 let positionX = 0;
 let positionY = 0;
 let incrementX = 25;
+let rotX, rotY;
 let positionXhead = 175;
 let positionYhead = 50;
 snakeHead.style.top = positionYhead
 snakeHead.style.left = positionXhead
 snakeHead.style.transform = 'rotate(90deg)'
+
+
 let direction = 'R';
 let keyPress = '';
 snakeSegments.forEach((item, index) =>{
@@ -70,13 +73,13 @@ function moveLeft(head){
 }
 
 function moveRight(head){
-    segmentHeadX = head.style.left
+    segmentHeadX = head.style.left 
     segmentHeadX = parseInt(segmentHeadX.match(/\d+/))
     segmentHeadX += 25
     head.style.left = `${segmentHeadX}px`;
 }
 const moveSnake = () => {
-     
+    
     document.addEventListener('keydown',e=>{
         if(e.key === "Escape"){
             cycleGame = false;
@@ -96,25 +99,36 @@ const moveSnake = () => {
     
 
     intervalBegin = setInterval(()=>{
+        
         if(cycleGame){
             
             
             let segmentX;
+            let segmentY;
+            //snakeHead.style.left = `${snakeHead.style.left + 25}px`;
+            let prevX = snakeHead.style.left;
+            let prevY = snakeHead.style.top;
+
+            let prev2X, prev2Y;
             snakeSegments.forEach((item,index)=>{
                 //const {width, height} = item.getBoundingClientRect();
-
+                prev2X = item.style.left;
+                prev2Y = item.style.top;
+                item.style.left = prevX;
+                item.style.top = prevY;
+                prevX = prev2X;
+                prevY = prev2Y;    
+            
                 // Save the current coordinates of the snake's head
-                let prevX = positionXhead; 
-                let prevY = positionYhead;
-                
-                let prev2X, prev2Y;
                 let segmentHeadX, segmentHeadY;
                 // Movement of the snake's head depending on the direction
                 if(index === 0 && direction){
+                    
                     if (direction === 'R') {
                         segmentX = item.style.left
                         segmentX = parseInt(segmentX.match(/\d+/))
-                        item.style.left = `${segmentX+incrementX}px`
+                        item.style.left = `${segmentX + incrementX}px`
+                        
                             
                     }
                     switch (keyPress) {
@@ -137,6 +151,8 @@ const moveSnake = () => {
                         case 'ArrowDown':
                                 if(direction != 'U'){
                                     moveDown(item)
+                                    rotX = item.style.left
+                                    rotY = item.style.top
                                     item.style.transform = 'rotate(180deg)'
                                     direction = 'D'
                                 }else if (direction == 'U') {
@@ -164,12 +180,9 @@ const moveSnake = () => {
                                 }
                                 break;
                     }
-                }else{
-                    segmentX = item.style.left
-                    segmentX = parseInt(segmentX.match(/\d+/))
-                    item.style.left = `${segmentX+incrementX}px`
-                        
                 }
+
+                
 /*
                 // Snake body update
                 for (let i = 1; i < snakeLength; i++) {
@@ -183,11 +196,40 @@ const moveSnake = () => {
 */
             
             })    
+            /*
+            for(let i = 0; i < snakeLength; i++){
+                /*
+                segmentX = snakeSegments[i-1].style.left
+                segmentY = snakeSegments[i-1].style.top
+                segmentX = parseInt(segmentX.match(/\d+/))
+                segmentY = parseInt(segmentY.match(/\d+/))
+                if(direction == 'R'){
+                    snakeSegments[i].style.left = `${segmentX-25}px` 
+                    
+                }else if(direction == 'D'){
+                   
+                        snakeSegments[i].style.top = `${segmentY-25}px` 
+                        snakeSegments[i].style.left = `${segmentX-25}px` 
+                    
+                        
+                   
+                   //snakeSegments[i].style.left = `${segmentX-25}px` 
+                    //snakeSegments[i].style.top = `${segmentY+25}px` 
+                      
+                }
                 
-            }
+                //snakeSegments[i].style.top = `${segmentY}px` 
+                
+                    
         
+                //snakeSegments[i-1].style.left = `${segmentX+incrementX}px` 
+            }*/
+            
+            }
+            
 
     }, 300)
+    
 /*
     intervalHead = setInterval(()=>{
         if(cycleGame){
